53645134b31eca7276bb055fb0fd03b7de72a48f
pratos <pratos9@live.com>
2017-04-06 11:32:07 +0530

    environment file

--- python_predict_client/predict_client.py
+++ python_predict_client/predict_client.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
-import numpy
+"""Send JPEG image to tensorflow_model_server loaded with inception model.
+"""
+
+from __future__ import print_function
+
+# This is a placeholder for a Google-internal import.
 
 from grpc.beta import implementations
 import tensorflow as tf
@@ -8,44 +13,29 @@ import tensorflow as tf
 import predict_pb2
 import prediction_service_pb2
 
-tf.app.flags.DEFINE_string("host", "127.0.0.1", "gRPC server host")
-tf.app.flags.DEFINE_integer("port", 9000, "gRPC server port")
-tf.app.flags.DEFINE_string("model_name", "cancer", "TensorFlow model name")
-tf.app.flags.DEFINE_integer("model_version", -1, "TensorFlow model version")
-tf.app.flags.DEFINE_float("request_timeout", 10.0, "Timeout of gRPC request")
+
+tf.app.flags.DEFINE_string('server', '104.197.123.248:9000',
+                           'PredictionService host:port')
+tf.app.flags.DEFINE_string('image', '', 'path to image in JPEG format')
 FLAGS = tf.app.flags.FLAGS
 
 
-def main():
-  host = FLAGS.host
-  port = FLAGS.port
-  model_name = FLAGS.model_name
-  model_version = FLAGS.model_version
-  request_timeout = FLAGS.request_timeout
-
-  # Generate inference data
-  keys = numpy.asarray([1, 2, 3])
-  keys_tensor_proto = tf.contrib.util.make_tensor_proto(keys, dtype=tf.int32)
-  features = numpy.asarray(
-      [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 1, 1, 1, 1, 1, 1, 1, 1],
-       [9, 8, 7, 6, 5, 4, 3, 2, 1], [9, 9, 9, 9, 9, 9, 9, 9, 9]])
-  features_tensor_proto = tf.contrib.util.make_tensor_proto(features,
-                                                            dtype=tf.float32)
-
-  # Create gRPC client and request
-  channel = implementations.insecure_channel(host, port)
+def main(_):
+  host, port = FLAGS.server.split(':')
+  channel = implementations.insecure_channel(host, int(port))
   stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
-  request = predict_pb2.PredictRequest()
-  request.model_spec.name = model_name
-  if model_version > 0:
-    request.model_spec.version.value = model_version
-  request.inputs['keys'].CopyFrom(keys_tensor_proto)
-  request.inputs['features'].CopyFrom(features_tensor_proto)
-
   # Send request
-  result = stub.Predict(request, request_timeout)
-  print(result)
+  with open(FLAGS.image, 'rb') as f:
+    # See prediction_service.proto for gRPC request/response details.
+    data = f.read()
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'inception'
+    request.model_spec.signature_name = 'predict_images'
+    request.inputs['images'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(data, shape=[1]))
+    result = stub.Predict(request, 1200.0)  # 10 secs timeout
+    print(result)
 
 
 if __name__ == '__main__':
-  main()
+  tf.app.run()
--- python_predict_client/predict_pb2.py
+++ python_predict_client/predict_pb2.py
@@ -225,9 +225,14 @@ _PREDICTREQUEST_INPUTSENTRY.has_options = True
 _PREDICTREQUEST_INPUTSENTRY._options = _descriptor._ParseOptions(descriptor_pb2.MessageOptions(), _b('8\001'))
 _PREDICTRESPONSE_OUTPUTSENTRY.has_options = True
 _PREDICTRESPONSE_OUTPUTSENTRY._options = _descriptor._ParseOptions(descriptor_pb2.MessageOptions(), _b('8\001'))
-import grpc
-from grpc.beta import implementations as beta_implementations
-from grpc.beta import interfaces as beta_interfaces
-from grpc.framework.common import cardinality
-from grpc.framework.interfaces.face import utilities as face_utilities
+try:
+  # THESE ELEMENTS WILL BE DEPRECATED.
+  # Please use the generated *_pb2_grpc.py files instead.
+  import grpc
+  from grpc.framework.common import cardinality
+  from grpc.framework.interfaces.face import utilities as face_utilities
+  from grpc.beta import implementations as beta_implementations
+  from grpc.beta import interfaces as beta_interfaces
+except ImportError:
+  pass
 # @@protoc_insertion_point(module_scope)
